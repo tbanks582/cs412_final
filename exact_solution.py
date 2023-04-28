5d2166383ffb59d435dab603bc57f93a5224b59a
trevor nicholas <nicholtb@dukes.jmu.edu>
2023-04-28 11:09:42 -0400

    completed exact solution

--- exact_solution.py
+++ exact_solution.py
@@ -1,16 +1,49 @@
 def main():
-    print('asd123')
+    # print('asd123')
     num_vertices, num_edges = [int(x) for x in input().split()]
     graph = {}
     for i in range(num_vertices):
         graph[i] = set()
     for _ in range(num_edges):
         u, v, w = [int(x) for x in input().split()]
+        # might need to add edges going the other way too
         graph[u].add((v,w))
-    verify([0,1,3,2,4,5], 70, graph)
+    sols = []
+    cost, path = longest(graph, [], 0, sols)
+    print(cost, path)
+    verify(path, cost, graph)
+    # verify([0,1,3,2,4,5], 70, graph)
+    
     print(graph)
 
 
+def longest(graph, path,cost, sols):
+    maxP = path
+    maxC = cost
+    for v in graph:
+        c, p = helper(graph, [v], 0, sols)
+        # print(v, c,p)
+        if c > maxC:
+            maxC = c
+            maxP = p
+    maxC, maxP = max(sols)
+    return maxC, maxP
+    
+def helper(graph, path, cost, sols):
+    newCost = cost
+    newPath = path
+    for adj, weight in graph[path[-1]]:
+        newCost = cost
+        newPath = path
+        # print(path, adj)
+        if adj not in path:
+            c, p = helper(graph, path + [adj], cost+weight, sols)
+            if c>newCost:
+                newCost = c
+                newPath = p
+    # print(newCost, newPath)
+    sols.append((newCost, newPath))
+    return newCost, newPath
 
 def verify(path, weight, graph):
     total = 0
