b73975d6c1409067b78593d37c139ee782efb864
Kiavash Seraj <serajkx@dukes.jmu.edu>
2023-04-28 23:29:13 -0400

    adding annealing approx algorithm

--- approximation_greedy.py
+++ approximation_greedy.py
@@ -1,3 +1,5 @@
+import random
+
 def main():
     num_vertices, num_edges = [int(x) for x in input().split()]
     graph = {}
@@ -6,9 +8,38 @@ def main():
     for _ in range(num_edges):
         u, v, w = [int(x) for x in input().split()]
         graph[u].add((v,w))
-    longest_path_greedy(graph, 1)
+
+    # random_key = random.choice(list(graph.keys()))
+    # print(random_key)
+    # longest_path_greedy(graph, random_key)
+    print(graph)
+    generate_random_path(graph)
 
 
+def generate_random_path(graph):
+    marked = set()
+    start = random.choice(list(graph.keys()))
+
+    path = [start]
+    marked.add(start)
+
+    next_node = random.choice(list(graph[start]))
+    marked.add(next_node)
+
+    while next_node is not None:
+        path.append(next_node)
+        random_neighbor = random.choice(list(graph[start]))
+        if random_neighbor not in marked:
+            next_node = random_neighbor
+    return path
+
+
+
+# def annealing_longest_path(graph, inital_temp, cool_rate, stop_temp, path):
+
+
+
+# Find the longest path by taking the a verticies heaviest edge rinse and repreat
 def longest_path_greedy(graph, start):
     marked = set()
     curr = start
@@ -33,9 +64,8 @@ def longest_path_greedy(graph, start):
         curr = next
     
     print("Length of the longest path is approximately ", path_length)
-    print("Path was: ")
 
-    output = ""
+    output = "Path was: "
     for i in range(len(longest_path_edges)):
         if i == len(longest_path_edges) - 1:
             output = output + str(longest_path_edges[i])
